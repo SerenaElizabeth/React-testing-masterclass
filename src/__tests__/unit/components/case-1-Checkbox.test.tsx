191bfb67c73b6f7e6b5334c5d381c4b014b0f907
Serena Graham <serenaelizabeth@hotmail.co.uk>
2021-05-06 13:47:46 +0100

    case 2

--- src/__tests__/unit/components/case-1-Checkbox.test.tsx
+++ src/__tests__/unit/components/case-1-Checkbox.test.tsx
@@ -1,23 +1,44 @@
 import React from 'react'
-import { axe } from 'jest-axe'
 import { render, fireEvent } from '@testing-library/react'
 
 import Checkbox from '../../../components/Checkbox'
 
-/**
- * This checkbox component renders a checkbox with a label.
- * Since we customized the default checkbox, we want to
- * make sure it still works as a regular checkbox
- * should.
- */
+const defaultCheckboxProps = {
+  label: 'TEST_CHECKBOX_LABEL',
+  id: 'TEST_CHECKBOX_ID',
+  onChange: jest.fn(),
+  borderColor: '#fff',
+  checked: false,
+  background: '#000',
+  checkMarkBackground: '#fff',
+}
+
 describe('The <Checkbox /> component', () => {
-  it('❌ Should render the label and checkbox the user will see', () => {})
+  it('Should render the label and checkbox the user will see', () => {
+    //render a checkbox. getByLabelText is from the library
+    const { getByLabelText } = render(<Checkbox {...defaultCheckboxProps} />)
 
-  it('❌ Should make the checkbox accessible by setting the id and htmlFor attributes on label and checkbox', () => {})
+    //we are expecting the element with this label to be in the doc
+    expect(getByLabelText('TEST_CHECKBOX_LABEL')).toBeInTheDocument()
+  })
 
-  it('❌ Should call the onChange handler when it is provided', () => {})
+  it('Should call the onChange handler when it is provided', () => {
+    //render a checkbox
+    const { getByLabelText } = render(<Checkbox {...defaultCheckboxProps} />)
+    //access the checkbox using the method getByLabelText and then save this to variable called checkbox
+    const checkbox = getByLabelText(defaultCheckboxProps.label)
+    //we need to use the fireEvent method to simulate a click
+    fireEvent.click(checkbox)
+    //expect onChange to be called
+    expect(defaultCheckboxProps.onChange).toHaveBeenCalled()
+  })
 
-  it('❌ Should change state correctly when clicked (checked and unchecked)', () => {})
-
-  it('❌ should not fail any accessibility tests', async () => {})
+  it('Should change state correctly when clicked (checked and unchecked)', () => {
+    //render a checkbox
+    const { getByLabelText } = render(
+      <Checkbox {...{ ...defaultCheckboxProps, checked: true }} />,
+    )
+    //access the checkbox using the method getByLabelText and then save this to variable called checkbox
+    expect(getByLabelText(defaultCheckboxProps.label)).toBeChecked()
+  })
 })
--- src/__tests__/unit/components/case-2-ProductTile.test.tsx
+++ src/__tests__/unit/components/case-2-ProductTile.test.tsx
@@ -1,18 +1,38 @@
 import React from 'react'
 import { axe } from 'jest-axe'
-import { render } from '@testing-library/react'
+import { getByAltText, render } from '@testing-library/react'
 import ProductTile from '../../../components/ProductTile'
 
+const testProps = {
+  id: 12,
+  name: 'Test Name',
+  price: '$12.99',
+  image: '/image.png',
+  brand: 'Test Brand',
+  createdAt: '2020-02-11 00:00:00',
+  isActive: true,
+}
+
 describe('The <ProductTile /> component', () => {
-  it('❌ renders a product tile with name, image and price', () => {
+  it('renders a product tile with name, image and price', () => {
+    //render the product tile
+    //as any tells typescript that we don't need to pass in all the props
+    const { getByText, getByAltText } = render(
+      <ProductTile {...(testProps as any)} />,
+    )
 
+    //expect element with name 'Test Name' to be in the doc
+    expect(getByText(testProps.name)).toBeInTheDocument()
+    expect(getByText(testProps.price)).toBeInTheDocument()
+    expect(getByAltText(testProps.name)).toBeInTheDocument()
   })
 
-  it('❌ renders a product tile with name and price only', () => {
-
-  })
-
-  it('❌ has no accessibility violations', async () => {
-
+  it('renders a product tile with name and price only', () => {
+    //render a product tile with no image as a prop
+    const { queryByAltText } = render(
+      <ProductTile {...({ ...testProps, image: undefined } as any)} />,
+    )
+    //we are expecting the alt text for the image to be null because the image is condiionally rendered
+    expect(queryByAltText(testProps.name)).toBeNull()
   })
 })
